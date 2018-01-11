78ddd07fca28dda7305357a4878c9132fd60369f
Max Starikevich <starikevich@newsite.by>
2018-01-11 20:51:29 +0300

    Added deployment script

---
+++ deploy.php
@@ -0,0 +1,21 @@
+<pre>
+	<?php
+		$branch = 'master';
+
+		$commands = [
+			'git add --all',
+			'git commit -m "Changes on production"',
+			"git pull origin {$branch}",
+			'git checkout --theirs .',
+			'git commit -am "Remote Conflict"',
+			"git push origin {$branch}",
+			"yarn build"
+		];
+
+		foreach($commands as $command) {
+			$tmp = shell_exec($command." 2>&1 ");
+			echo '<b>' . $command . '</b>' . PHP_EOL;
+			echo htmlentities(trim($tmp)) . PHP_EOL . PHP_EOL;
+		}
+	?>
+</pre>
